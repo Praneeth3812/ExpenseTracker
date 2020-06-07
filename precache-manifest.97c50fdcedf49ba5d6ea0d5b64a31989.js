b2ac8b574ffcf8f83aa78c65bc9f8ca2aa753485
PraneethNamala <praneeth.namala@credencys.com>
2020-06-07 15:07:30 +0530

    Updates

---
+++ precache-manifest.97c50fdcedf49ba5d6ea0d5b64a31989.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9cab3700d04764acb8a11c9e29c6ce3",
+    "url": "/ExpenseTracker/index.html"
+  },
+  {
+    "revision": "984f81d3616d9dfea8ab",
+    "url": "/ExpenseTracker/static/css/main.d0e32a8e.chunk.css"
+  },
+  {
+    "revision": "52be1f5d5c695c93f168",
+    "url": "/ExpenseTracker/static/js/2.425c6326.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ExpenseTracker/static/js/2.425c6326.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "984f81d3616d9dfea8ab",
+    "url": "/ExpenseTracker/static/js/main.49905eb7.chunk.js"
+  },
+  {
+    "revision": "6a4761a450062a671a7d",
+    "url": "/ExpenseTracker/static/js/runtime-main.de1c3c37.js"
+  }
+]);
